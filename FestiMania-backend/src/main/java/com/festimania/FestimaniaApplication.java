9a4cfbc4220e57179ed909f545917a84f759323e
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-09-25 11:07:45 +0200

    inicio del backend de FestiMania - Backend

---
+++ FestiMania-backend/src/main/java/com/festimania/FestimaniaApplication.java
@@ -0,0 +1,13 @@
+package com.festimania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestimaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestimaniaApplication.class, args);
+	}
+
+}
---
+++ FestiMania-backend/src/test/java/com/festimania/FestimaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festimania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestimaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
